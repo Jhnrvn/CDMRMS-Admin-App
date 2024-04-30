4ee9bd50f093f3249b0fc4bdb5655d5675c99752
John Irvin Geanga <johnirvingeanga@gmail.com>
2024-04-30 09:53:47 +0800

    database backup

--- Database/cdmregistrarmanagementsystem.sql
+++ Database/cdmregistrarmanagementsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2024 at 06:16 AM
+-- Generation Time: Apr 30, 2024 at 03:47 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,11 +42,15 @@ CREATE TABLE `assignedcourse` (
 --
 
 INSERT INTO `assignedcourse` (`id`, `instructor_id`, `Instructor`, `course`, `section_1`, `section_2`, `section_3`) VALUES
-(1, 'CDM-1111', '', 'IT ELECT', '', '', ''),
+(1, 'CDM-1111', 'Geanga, John Irvin C.', 'IT ELECT', 'BSIT-2A', 'BSIT-2B', 'BSIT-2C'),
 (2, 'CDM-2222', '', 'SSDEV', '', '', ''),
 (3, 'CDM-3333', '', 'FUDBS', '', '', ''),
 (4, 'CDM-4444', '', 'IP TECH', '', '', ''),
-(5, 'CDM-4444', '', 'ADM', '', '', '');
+(5, 'CDM-4444', '', 'ADM', '', '', ''),
+(6, 'CDM-1010', '', 'PROG 2', '', '', ''),
+(7, 'CDM-1111', 'Geanga, John Irvin C.', 'SIA 2', '', '', ''),
+(8, 'CDM-1111', 'Geanga, John Irvin C.', 'GE 1', '', '', ''),
+(9, 'CDM-1111', 'Geanga, John Irvin C.', 'GE 4', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -80,7 +84,8 @@ INSERT INTO `instructors` (`id`, `firstname`, `middlename`, `lastname`, `instruc
 (6, 'Silvia', 'N/A', 'Marty', 'CDM-6666', 'Female', 'sample@gmail.com', '09666666666', '2024-04-26', '938db8c9f82c8cb58d3f3ef4fd250036a48d26a712753d2fde5abd03a85cabf4'),
 (7, 'Edith', 'N/A', 'Bernadette', 'CDM-7777', 'Female', 'sample@gmail.com', '09777777777', '2024-04-26', '938db8c9f82c8cb58d3f3ef4fd250036a48d26a712753d2fde5abd03a85cabf4'),
 (8, 'Cydney', 'N/A', 'Kris', 'CDM-8888', 'Female', 'sample@gmail.com', '09888888888', '2024-04-26', '938db8c9f82c8cb58d3f3ef4fd250036a48d26a712753d2fde5abd03a85cabf4'),
-(9, 'Catherin', 'N/A', 'Shonda', 'CDM-9999', 'Female', 'sample@gmail.com', '09999999999', '2024-04-26', '938db8c9f82c8cb58d3f3ef4fd250036a48d26a712753d2fde5abd03a85cabf4');
+(9, 'Catherin', 'N/A', 'Shonda', 'CDM-9999', 'Female', 'sample@gmail.com', '09999999999', '2024-04-26', '938db8c9f82c8cb58d3f3ef4fd250036a48d26a712753d2fde5abd03a85cabf4'),
+(14, 'Max', 'N/A', 'Collins', 'CDM-1010', 'Male', 'sample@gmail.com', '09101010101', '2024-04-28', '938db8c9f82c8cb58d3f3ef4fd250036a48d26a712753d2fde5abd03a85cabf4');
 
 --
 -- Indexes for dumped tables
@@ -106,13 +111,13 @@ ALTER TABLE `instructors`
 -- AUTO_INCREMENT for table `assignedcourse`
 --
 ALTER TABLE `assignedcourse`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `instructors`
 --
 ALTER TABLE `instructors`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
