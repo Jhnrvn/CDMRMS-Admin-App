f6388736f89174a7dca9e5e09eb40995ea2a0fa2
John Irvin Geanga <johnirvingeanga@gmail.com>
2024-07-06 22:17:31 +0800

    UI Update

--- CDMRMS_ADMINISTRATOR/My Project/Resources.Designer.vb
+++ CDMRMS_ADMINISTRATOR/My Project/Resources.Designer.vb
@@ -63,16 +63,6 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property Arrow_Right() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("Arrow_Right", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
         Friend ReadOnly Property Assigned_Button_Icon() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Assigned_Button_Icon", resourceCulture)
